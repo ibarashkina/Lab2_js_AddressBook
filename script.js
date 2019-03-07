b9aae46f063fb7acc4b17b54da0979a36b10a84c
Ilona <ilonabarashkina@gmail.com>
2019-03-07 17:56:12 -0500

    Made a few changes

--- script.js
+++ script.js
@@ -11,28 +11,27 @@
         }
 
         display () {
-            // this is the section we are targetting
-            // The dot notation creates/mimics the path of what info you want to retrieve
+           
             let contact_section = document.getElementById("contact_section");
 
-            // Setting up variables for creating elements
+            
             let div = document.createElement("div");
             let info = document.createElement("p");
             let icon = document.createElement("i");
 
-            // Adding CSS classes to created elements
+            
             div.classList.add("contact_container");
             icon.classList.add("material-icons");
             icon.classList.add("delete");
 
-            //Adding text
+            
             icon.innerText ="delete";
             info.innerText = `Name: ${name.value}
             Email: ${email.value}
             Phone: ${phone.value}
             Relation: ${relation.value}`;
 
-            //Appending info to div,and div to contact section
+            
             div.appendChild(info);
             div.appendChild(icon);
             contact_section.appendChild(div);
@@ -48,7 +47,7 @@
         }
 
     }
-    //This allows to add contact info
+    
         class Contact {
         constructor(name, email, phone, relation) {
             this.name = name;
